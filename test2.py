981bf52981e3b7d962104cd857d6abd376e9d458
d0ct0rstrange <iamathulas@gmail.com>
2021-01-04 02:29:51 +0530

    Webscrape.py progressing. Finished (Corrected) checkresultcount() in webscrape.py

--- test2.py
+++ test2.py
@@ -1,9 +1,28 @@
+import dateutil as dparser
+from dateutil import parser
 import concurrent.futures
-import db
+import db,string_clean
+
+ax="1"
+
+#with concurrent.futures.ThreadPoolExecutor() as executor1:
+#    t1=executor1.submit(db.execute_query,"select * from courses")
+#    res=t1.result()
 
 
-with concurrent.futures.ThreadPoolExecutor() as executor1:
-    t1=executor1.submit(db.execute_query,"select * from courses")
-    res=t1.result()
+#print(res)
+a="Published on .21/12/2020"
+b="Published on 22/12/2020"
+c="Published on 1/1/2021"
+d="21/12/2020"
 
-print(res)
+res=string_clean.similarity_between_strings(a,d)
+str_res=str(res)
+if(res<=0.60 and res>=0.58):
+    print(res)
+else:
+    print("Not correct. Result:"+str_res)
+
+
+resu=parser.parse("monkey 21/12/2020 love banana",fuzzy=True)
+print(resu.date())
--- webscrape.py
+++ webscrape.py
@@ -8,11 +8,13 @@ from difflib import SequenceMatcher
 import concurrent.futures
 from datetime import datetime
 import datefinder
+from dateutil import parser
 
 
 import csvwrite,string_clean
 import csv
 import db
+
 
 #New file to rewrite result scraping functions
 
@@ -28,29 +30,90 @@ secondrecentdate=[] #String of secondrecentdate
 srd=[]              #Date object of secondrecentdate
 status=query=''
 savedcount=0
+currcount=0
 
-#Function to check the new results count.
-# Input i 
+# Function to check the new results count.
+# Input webpage(out=r.text), query and unique classname of result
+# Function fetches data from table data and returns number of results between
+# recentdate and secondrecentdate saved in table. 
+ 
 def checkresultcount(out,query="Published on ",classname="displayList"): 
     conn=db.init_conn()
+    global savedcount,recentdate, secondrecentdate #Declaring vars as global
     with concurrent.futures.ThreadPoolExecutor() as executor1:
         t1=executor1.submit(db.execute_query_thread,"select * from data")
         res=t1.result()
     
 
-
+    # Fetching information from table data
     for row in res:
-        a=row[0] #recentdate
-        b=row[1] #secondrecentdate
-        global savedcount
-        savedcount=row[3] #count
+        a=row[0] #recentdate from data table
+        b=row[1] #secondrecentdate from data table
+        savedcount=row[3] #count from data table (Saving as global)
         
+        recentdate=a       # Saving recentdate from database to global var
+        secondrecentdate=b # Saving secondrecentdate from database to global var
+
     #c=out.find("Published on "+a) #Start region from rd
     #d=out.find("Published on "+b) #Stop region from srd
 
+    res=re.search(query,out)
+    status=res.group(0)
+    substring=re.findall(r'>(.*?)<',out)
+
+    #Fetches all dates the results published from result page. Identified by query. (published on)
+    alldates = [idx for idx in substring if idx.lower().startswith(query.lower())]
+
+    #Converting recentdate to date object rd and
+    #Converting secondrecentdate to date object srd 
+    matches = list(datefinder.find_dates(recentdate))
+    if len(matches) > 0:
+        # date returned will be a datetime.datetime object. here we are only using the first match.
+        rd = matches[0]
+        rd=rd.date()
+        #print(rd.date())
+    else:
+        print('No rd dates found')
+    matches = list(datefinder.find_dates(secondrecentdate))
+    if len(matches) > 0:
+        # date returned will be a datetime.datetime object. here we are only using the first match.
+        srd = matches[0]
+        srd=srd.date()
+        #print(srd.date())
+    else:
+        print('No srd dates found')
+
+    curr_recentdate=alldates[0]
+    curr_secondrecentdate=alldates[1]
+
+    
+    conv_curr_recentdate=parser.parse(curr_recentdate,fuzzy=True)
+
+    conv_curr_secondrecentdate=parser.parse(curr_secondrecentdate,fuzzy=True)
+
+
+    # If Current recentdate is same as recentdate in data (savedrecentdate)
+    # ROI[start]=curr_recentdate and ROI[end]=curr_secondrecentdate  
+    if(conv_curr_recentdate==rd):
+        a=curr_recentdate
+        b=curr_secondrecentdate
+        #TODO: Write db.data.count=>0
+        # and return the curr_count
+
+    # If current_recentdate is not same as savedrecentdate,
+    # ROI[start]=curr_recentdate and ROI[end]=savedrecentdate  
+    elif(conv_curr_recentdate!=rd):
+        a=curr_recentdate
+        b=string_clean.normalizeDate(srd)
+        
+        #TODO: Write db.data.count=>0
+        # and return the curr_count
+
+    # Finding Region Of Interest
     c=out.find(query+a) #Start region from rd
     d=out.find(query+b) #Stop region from srd
 
+    #Failsafe.
     if c>d:
         roi=out[d:c] #Region Of Interest between srd and rd
     elif d>c:
@@ -62,6 +125,8 @@ def checkresultcount(out,query="Published on ",classname="displayList"):
 
     return count
 
+#Function to fetch web page and check results count.
+# Depends on checkresultcount() function
 def fetch_results_count(classname="displayList",query="Published on ",url='https://exams.keralauniversity.ac.in/Login/check8'):
     #1. URL => URL from the results to be fetched
     #          ('https://exams.keralauniversity.ac.in/Login/check8' is the default)
@@ -73,7 +138,7 @@ def fetch_results_count(classname="displayList",query="Published on ",url='https
     #3. CLASSNAME => The class name for new result
     #                ("displayList" is the default)
 
-    global recentdate, secondrecentdate,rd,srd,out,savedcount
+    global recentdate, secondrecentdate,rd,srd,out,savedcount,currcount
     
     #Fetching webpage from url
     r = requests.get(url)
@@ -127,7 +192,11 @@ def fetch_results_count(classname="displayList",query="Published on ",url='https
 def fetch_results(classname="displayList",query="Published on ",url='https://exams.keralauniversity.ac.in/Login/check8'):
     # We have fetched the new results count using fetch_results_count. Now, we want to fetch the new results
     # To do so, first select recentdate,secondrecentdate from data;
-    # Find ROI between 
+    # This function will only be called when fetch_results_count returns num > 0
+    # This function will fetch results between recentdate and secondrecentdates saved in table data
+
+    # So, to fetch results, we need to fetch results between recentdate and secondrecentdate
+    # Find ROI between those dates from input "out(r.text)".
 
 
     #1. URL => URL from the results to be fetched
@@ -140,7 +209,7 @@ def fetch_results(classname="displayList",query="Published on ",url='https://exa
     #3. CLASSNAME => The class name for new result
     #                ("displayList" is the default)
 
-    global recentdate, secondrecentdate,rd,srd,out,savedcount
+    global recentdate, secondrecentdate,rd,srd,out,savedcount,currcount
     
     #Fetching webpage from url
     r = requests.get(url)
@@ -158,12 +227,17 @@ def fetch_results(classname="displayList",query="Published on ",url='https://exa
     alldates = [idx for idx in substring if idx.lower().startswith(query.lower())]
 
     #Recently published data
-    recentdate= alldates[0]
-    secondrecentdate=alldates[1]
+    #recentdate= alldates[0]
+    #secondrecentdate=alldates[1]
 
     #Finds current time to update the Last Fetch time
     now=datetime.now()
+
+    #Checks if recentdate is found in webpage
     matches = list(datefinder.find_dates(recentdate))
+
+    #If recentdate is found in webpage, then convert the date string to date object
+    # i.e. recentdate to rd. 
     if len(matches) > 0:
         # date returned will be a datetime.datetime object. here we are only using the first match.
         rd = matches[0]
@@ -171,7 +245,12 @@ def fetch_results(classname="displayList",query="Published on ",url='https://exa
         #print(rd.date())
     else:
         print('No rd dates found')
+    
+    #Checks if secondrecentdate is found in webpage
     matches = list(datefinder.find_dates(secondrecentdate))
+    
+    #If secondrecentdate is found in webpage, then convert the date string to date object
+    # i.e. secondrecentdate to rd. 
     if len(matches) > 0:
         # date returned will be a datetime.datetime object. here we are only using the first match.
         srd = matches[0]
@@ -180,9 +259,19 @@ def fetch_results(classname="displayList",query="Published on ",url='https://exa
     else:
         print('No srd dates found')
 
+    # Now, we can decide where to fetch results
+
+    conv_recentdate=datetime.datetime.strptime(recentdate,'%d/%m/%Y')
+
+# If recentdate from data is same as new recent date
+    #if(conv_recentdate==rd and currcount>savedcount ):
+
 
     
+
+
     
+# TODO: Function to update results    
 
 
 fetch_results_count()
